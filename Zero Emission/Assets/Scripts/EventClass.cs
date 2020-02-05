679e3987235c1f85ed323975443bf6451d27b149
Trollsnor <sofiachylekauko@gmail.com>
2020-02-05 18:03:27 +0100

    new event system "imported"
    experimenting with unitys own eventSystem is not an option because it seems to not be able to pass parameters when event is fired.

---
+++ Zero Emission/Assets/Scripts/EventClass.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// use as a parent to all events created. 
+public class EventClass : MonoBehaviour
+{
+
+    public string Description;
+
+    public EventClass(string description)
+    {
+        this.Description = description;
+    }
+
+}
