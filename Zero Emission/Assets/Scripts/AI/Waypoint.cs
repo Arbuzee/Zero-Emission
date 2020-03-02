0e005a420265566a59ce48b63fc4788fb057ebff
Barburat <38035810+Barburat@users.noreply.github.com>
2020-03-02 12:27:14 +0100

    Waypoints + AI stuff

--- Zero Emission/Assets/Scripts/AI/Waypoint.cs
+++ Zero Emission/Assets/Scripts/AI/Waypoint.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class Waypoint : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Waypoint previousWaypoint;
+    public Waypoint nextWaypoint;
 
-    // Update is called once per frame
-    void Update()
+    [Range(0f, 5f)]
+    public float width = 1f;
+
+    public Vector3 GetPosition()
     {
-        
+        Vector3 minBound = transform.position + transform.right * width / 2f;
+        Vector3 maxBound = transform.position - transform.right * width / 2f;
+        return Vector3.Lerp(minBound, maxBound, Random.Range(0f, 1f));
     }
 }
--- Zero Emission/Assets/Scripts/AI/WaypointManagerWindow.cs
+++ Zero Emission/Assets/Scripts/AI/WaypointManagerWindow.cs
@@ -1,18 +1,60 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
-public class WaypointManagerWindow : MonoBehaviour
+public class WaypointManagerWindow : EditorWindow
 {
-    // Start is called before the first frame update
-    void Start()
+    [MenuItem("Tools/Waypoint Editor")]
+    public static void Open()
     {
-        
+        GetWindow<WaypointManagerWindow>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public Transform WaypointRoot;
+
+    private void OnGUI()
     {
-        
+        SerializedObject obj = new SerializedObject(this);
+
+        EditorGUILayout.PropertyField(obj.FindProperty("WaypointRoot"));
+
+        if(WaypointRoot == null)
+        {
+            EditorGUILayout.HelpBox("Root transform must be selected.", MessageType.Warning);
+        }
+        else
+        {
+            EditorGUILayout.BeginVertical("box");
+            DrawButtons();
+            EditorGUILayout.EndVertical();
+        }
+
+        obj.ApplyModifiedProperties();
+    }
+
+    private void DrawButtons()
+    {
+        if(GUILayout.Button("Create Waypoint"))
+        {
+            CreateWaypoint();
+        }
+    }
+
+    private void CreateWaypoint()
+    {
+        GameObject waypointObject = new GameObject("Waypoint" + WaypointRoot.childCount, typeof(Waypoint));
+        waypointObject.transform.SetParent(WaypointRoot, false);
+
+        Waypoint waypoint = waypointObject.GetComponent<Waypoint>();
+        if(WaypointRoot.childCount > 1)
+        {
+            waypoint.previousWaypoint = WaypointRoot.GetChild(WaypointRoot.childCount - 2).GetComponent<Waypoint>();
+            waypoint.previousWaypoint.nextWaypoint = waypoint;
+            waypoint.transform.position = waypoint.previousWaypoint.transform.position;
+            waypoint.transform.forward = waypoint.previousWaypoint.transform.forward;
+        }
+
+        Selection.activeGameObject = waypoint.gameObject;
     }
 }
