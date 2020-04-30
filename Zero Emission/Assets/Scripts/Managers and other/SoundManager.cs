b73d7ec064a0bc9d88f2e3a38679dcb4926aeb48
Jocke <jockeljung1@gmail.com>
2020-04-30 17:38:26 +0200

    Menu, music and Camera // Sofia :angel:
    MenuControl, sound manager and camera Improvement. The glide works better, no glide whn mouse is still or when cam is bouncing from border.

---
+++ Zero Emission/Assets/Scripts/Managers and other/SoundManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+
+    public static SoundManager Instance;
+
+    private AudioSource aSrc;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else {
+            Destroy(this);
+        }
+    }
+
+    private void Start()
+    {
+        aSrc = GetComponent<AudioSource>();
+    }
+
+    public void SetMusic(bool on) {
+        if (on)
+        {
+            aSrc.Play();
+        }
+        else {
+            aSrc.Stop();
+        }
+        
+    }
+
+
+}
---
+++ Zero Emission/Assets/Scripts/Managers and other/UIManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+
+    [SerializeField] private GameObject menu;
+
+    public void Reset()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void OpenMenu()
+    {
+        menu.SetActive(true);
+    }
+
+    public void CloseMenu()
+    {
+        menu.SetActive(false);
+    }
+
+    public void DisableMusic()
+    {
+        SoundManager.Instance.SetMusic(false);
+    }
+
+    public void EnableMusic() {
+        SoundManager.Instance.SetMusic(true);
+    }
+
+}
